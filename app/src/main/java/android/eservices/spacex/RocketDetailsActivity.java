fbb53ee5802956f0b01493f2e481a2e55e2b55e7
Thomas <legristhomaspro@gmail.com>
2021-04-05 19:31:26 +0200

    add: details activity

---
+++ app/src/main/java/android/eservices/spacex/RocketDetailsActivity.java
@@ -0,0 +1,11 @@
+package android.eservices.spacex;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toolbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RocketDetailsActivity extends AppCompatActivity {
+    private static final String ROCKET = "rocket";
+}
